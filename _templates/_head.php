ab2db6b5fabe8e2cb145ce111182d8c641e7f958
ABISHEK E <plrabiyt@gmail.com>
2023-06-26 17:17:11 +0000

    Update file _head.php

--- _templates/_head.php
+++ _templates/_head.php
@@ -10,7 +10,7 @@
     <link href="/app/assets/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.1.0/css/hover-min.css" integrity="sha512-glciccPoOqr5mfDGmlJ3bpbvomZmFK+5dRARpt62nZnlKwaYZSfFpFIgUoD8ujqBw4TmPa/F3TX28OctJzoLfg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <?if(file_exists($_SERVER['DOCUMENT_ROOT'].'/app/css/'.basename($_SERVER['PHP_SELF'],".php".".css"))){?>
-    <link href="/app/css/<?=basename($_SERVER['PHP_SELF'],".php")?>
+    <link href="/app/css/<?=basename($_SERVER['PHP_SELF'],".php")?>.css" rel="stylesheet">
     <?}?>
 
 </head>
