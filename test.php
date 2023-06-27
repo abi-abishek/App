5beb584bad9183b530edfb3dd7bc293cff93bf84
ABISHEK E <plrabiyt@gmail.com>
2023-06-27 05:42:08 +0000

    Update file test.php

--- test.php
+++ test.php
@@ -1,18 +1,23 @@
 <pre>
 <?php
+
+include "libs/load.php";
+
 //$cookie_name = "testscript";
 //$cookie_value = $_SERVER['REQUEST_URL'];
 //setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-print("_SERVER /n");
-print_r($_SERVER);
-print("_GET /n");
-print_r($_GET);
-print("_POST /n");
-print_r($_POST);
-print("_FILES /n");
-print_r($_FILES);
-print("_COOKIE /n");
-print_r($_COOKIE);
+// print("_SERVER /n");
+// print_r($_SERVER);
+// print("_GET /n");
+// print_r($_GET);
+// print("_POST /n");
+// print_r($_POST);
+// print("_FILES /n");
+// print_r($_FILES);
+// print("_COOKIE /n");
+// print_r($_COOKIE);
+
+signup("","","","");
 
 ?>
 </pre
