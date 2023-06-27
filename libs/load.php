f4fa13286e0a732aa198f3b04724bb4f20d9fde4
ABISHEK E <plrabiyt@gmail.com>
2023-06-27 05:31:00 +0000

    Update file load.php

--- libs/load.php
+++ libs/load.php
@@ -12,3 +12,33 @@ function validate_credentials($username, $password){
         return false;
     }
 }
+
+function signup($username, $password , $email , $phone)
+{
+    $servername = "mysql.selfmade.ninja";
+    $username = "abishek";
+    $password = "Abi12345";
+    $dbname = "abishek_db";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }else{
+        print("Connection Success");
+    }
+
+     $sql = "INSERT INTO `auth` (`username`, `password`, `email`, `phone`, `blocked`, `active`)
+     VALUES ('admin', 'admin', 'admin@gmail.com', '8428420769', '0', '1');";
+     $result=false;
+     if ($conn->query($sql) === true) {
+       $result=true;
+     } else {
+       $result=false;
+     }
+    
+     $conn->close();
+     return $result;
+    
+}
