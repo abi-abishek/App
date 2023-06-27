3031c01d0966887d1aa5ed4c88ebc237c8274bd2
ABISHEK E <plrabiyt@gmail.com>
2023-06-27 06:40:05 +0000

    Update file _signup.php

--- _templates/_signup.php
+++ _templates/_signup.php
@@ -1,10 +1,10 @@
 <?php
 
 $signup = false;
-if (isset($_POST['username']) and isset($_POST['password']) and isset($_POST['email']) and isset($_POST['phone'])){
+if (isset($_POST['username']) and isset($_POST['password']) and isset($_POST['email_address']) and isset($_POST['phone'])){
   $username = $_POST['username'];
   $password = $_POST['password'];
-  $email = $_POST['email'];
+  $email = $_POST['email_address'];
   $phone = $_POST['phone'];
   $result = signup($username, $password, $email, $phone);
   $signup = true;
