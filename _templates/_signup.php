73d8296a364bf671a0aa7661d39f203ff9a475f1
ABISHEK E <plrabiyt@gmail.com>
2023-06-27 06:37:10 +0000

    Update file _signup.php

--- _templates/_signup.php
+++ _templates/_signup.php
@@ -1,3 +1,40 @@
+<?php
+
+$signup = false;
+if (isset($_POST['username']) and isset($_POST['password']) and isset($_POST['email']) and isset($_POST['phone'])){
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $result = signup($username, $password, $email, $phone);
+  $signup = true;
+}
+?>
+
+<?php
+  if ($signup) {
+    if ($result){
+      ?>
+      <main class="container">
+        <div class="bg-light p-5 rounded mt-3">
+          <h1>Signup Success</h1>
+          <p class="load">Now you can login from <a href="/login.php">here</a>.</p>
+        </div>
+      </main>
+<?php
+    }else{
+      ?>
+  <main class="container">
+    <div class="bg-light p-5 rounded mt-3">
+      <h1>Signup Fail</h1>
+      <p class="load">Something went wrong</p>
+    </div>
+  </main>
+  <?php
+      }
+    }else{
+      ?>
+
 <main class="form-signup">
   <form method="post" action="test.php">
     <img class="mb-4" src="/app/assets/images/Electrogram.png" alt="" width="300" height="95">
@@ -28,9 +65,6 @@
     <button class="w-100 btn btn-lg btn-primary hvr-grow-rotate" type="submit">Sign in</button>
   </form>
 </main>
-
 <?php
-$username = $_POST['email_address'];
-$password = $POST['password'];
-echo $username;
-?>
+    }
+  ?>
